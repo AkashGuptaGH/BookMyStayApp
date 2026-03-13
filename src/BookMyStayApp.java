607d96e3dcc2b89afab8e5d3081b9d649cabea73
AkashGuptaGH <akashgupta6181@gmail.com>
2026-03-13 15:39:36 +0530

    UC1-Welcome Message
    Added class documentation and updated welcome message.

--- src/BookMyStayApp.java
+++ src/BookMyStayApp.java
@@ -1,4 +1,11 @@
-package PACKAGE_NAME;
-
+/**
+ * BookMyStayApp
+ * UC1: Application entry and welcome message
+ *
+ * @version 1.0
+ */
 public class BookMyStayApp {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Hotel Booking System v1.0");
+    }
 }
